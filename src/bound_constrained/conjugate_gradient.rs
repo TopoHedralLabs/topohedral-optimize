758bf47b028a28a02c25943cdb420138877bc1e8
John Ferguson <JAFerguson952@gmail.com>
2026-05-19 23:07:51 +0100

    wip

--- src/bound_constrained/conjugate_gradient.rs
+++ src/bound_constrained/conjugate_gradient.rs
@@ -3,25 +3,10 @@
 //! Longer description of module
 //--------------------------------------------------------------------------------------------------
 
-use crate::RealFn;
-
 //{{{ crate imports
-use super::common::Options as BoundConstrainedOptions;
-use crate::constraints::BoundsConstraints;
 //}}}
 //{{{ std imports
 //}}}
 //{{{ dep imports
 //}}}
 //--------------------------------------------------------------------------------------------------
-
-pub struct Options
-{
-    boundcon_opts: BoundConstrainedOptions,
-}
-
-struct ProjectedFunction<F: RealFn>
-{
-    fcn: F,
-    bounds: BoundsConstraints,
-}
