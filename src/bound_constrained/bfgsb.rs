dce11e0aa6730dca1d2a511815419a33c57b569e
John Ferguson <JAFerguson952@gmail.com>
2026-06-15 22:58:02 +0100

    wip

--- src/bound_constrained/bfgsb.rs
+++ src/bound_constrained/bfgsb.rs
@@ -3,15 +3,17 @@
 //! Longer description of module
 //--------------------------------------------------------------------------------------------------
 
+use std::default;
+
 //{{{ crate imports
-use crate::constraints::{BoundsConstraints, CauchyPathPoint};
-use crate::Vector;
+use crate::constraints::{BoundStatus, BoundsConstraints, CauchyPathPoint};
+use crate::{Matrix, Vector};
 //}}}
 //{{{ std imports
 //}}}
 //{{{ dep imports
 use topohedral_linalg::VecType::Col;
-use topohedral_linalg::VectorOps;
+use topohedral_linalg::{MatMul, VectorOps};
 use topohedral_tracing::trace_fn;
 //}}}
 //--------------------------------------------------------------------------------------------------
@@ -26,44 +28,73 @@ struct CauchyPoint
 #[trace_fn]
 fn cauchy_point(
     bounds: &BoundsConstraints,
-    x: &Vector,
-    g: &Vector,
+    xk: &Vector,
+    gk: &Vector,
+    bmatk: &Matrix,
 ) -> CauchyPoint
 {
-    let mut d = -g.clone();
-    let (active_set, inactive_set) = bounds.active_and_inactive_sets(x, Some(&d));
+    let mut dk = -gk.clone();
+    let (active_set, inactive_set) = bounds.active_and_inactive_sets(xk, Some(&dk));
 
     if inactive_set.is_empty()
     {
         return CauchyPoint {
-            cauchy_point: x.clone(),
-            cauchy_curvature: Vector::zeros_vec(x.len(), Col),
+            cauchy_point: xk.clone(),
+            cauchy_curvature: Vector::zeros_vec(xk.len(), Col),
             free_variables: Vec::new(),
         };
     }
 
-    for (vi, _) in active_set
+    let mut x_cauchy = Vector::zeros_vec(xk.len(), Col);
+    for (vi, bound_status) in active_set
     {
-        d[vi] = 0.0;
+        dk[vi] = 0.0;
+        match bound_status
+        {
+            BoundStatus::AtLower =>
+            {
+                x_cauchy[vi] = bounds.get_lower(vi).unwrap();
+            }
+            BoundStatus::AtUpper =>
+            {
+                x_cauchy[vi] = bounds.get_upper(vi).unwrap();
+            }
+            _ =>
+            {
+                panic!("Unexpected")
+            }
+        }
     }
 
-    let cauchy_path = bounds.cauchy_path(x, &d);
+    let z = Vector::zeros_vec(dk.len(), Col);
+    let bmatk_z = z.clone();
+    let bmatk_d = bmatk.matmul(&dk);
+
+    let mut df_dt = gk.dot(&dk);
+    let mut d2f_dt2 = (dk.dot(&bmatk.matmul(&dk))).max(f64::EPSILON);
+    let mut dalpha_min = -df_dt / d2f_dt2;
+
+    let cauchy_path = bounds.cauchy_path(xk, &dk);
+    let mut alpha_prev = 0.0;
+    let mut dir_cur = &dk;
 
     for i in 1..cauchy_path.len()
     {
         let CauchyPathPoint {
-            alpha: alpha1,
-            point: x1,
-            direction: d1,
-            variable_index: idx1,
-        } = &cauchy_path[i - 1];
-
-        let CauchyPathPoint {
-            alpha: alpha2,
-            point: x2,
-            direction: d2,
-            variable_index: idx2,
+            alpha: alphai,
+            point: xi,
+            direction: di,
+            variable_index: vi,
         } = &cauchy_path[i];
+
+        let d_alpha = alphai - alpha_prev;
+
+        if dalpha_min < d_alpha
+        {
+            x_cauchy = (xi + d_alpha * di).into();
+            break;
+        }
+        alpha_prev = *alphai;
     }
 
     todo!()
