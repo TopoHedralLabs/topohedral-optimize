294d7dcea106d8131fb31b2886c3fed85bf067f2
John Ferguson <JAFerguson952@gmail.com>
2026-07-16 13:44:17 +0100

    fmt

--- src/line_search/utils.rs
+++ src/line_search/utils.rs
@@ -213,9 +213,8 @@ pub fn quadcubmin<F: RealFn1>(
     c: f64,
     phi_c: f64,
 ) -> Option<(f64, f64)> {
-    let to_pair = |x: &Option<f64>| -> Option<(f64, f64)> {
-        x.as_ref().map(|alpha| (*alpha, f.eval(alpha)))
-    };
+    let to_pair =
+        |x: &Option<f64>| -> Option<(f64, f64)> { x.as_ref().map(|alpha| (*alpha, f.eval(alpha))) };
     let cubic_min_alpha = cubicmin3(a, phi_a, dphi_a, b, phi_b, c, phi_c);
     let quad_min1_alpha = quadmin(a, phi_a, dphi_a, b, phi_b);
     let quad_min2_alpha = quadmin(a, phi_a, dphi_a, c, phi_c);
